65fe59b4fb9e4d087c1d738e38ae609e9edccb3f
GiancarloSanchez26 <sanchezgiancarlo82@gmail.com>
2025-09-10 19:09:51 -0700

    Fix: Se agrego el parametro conjunto a las rutas

--- src/infrastructure/routes/ReporteGNRoutes.ts
+++ src/infrastructure/routes/ReporteGNRoutes.ts
@@ -7,19 +7,19 @@ export function createReporteGNRoutes(): Router {
 
   const controller = container.get<ReporteGNController>("ReporteGNController");
 
-  router.get("/acciones-de-personal", (req, res) =>
+  router.get("/acciones-de-personal/:conjunto", (req, res) =>
     controller.getAccionesDePersonal(req, res)
   );
 
-  router.get("/contratos", (req, res) => controller.getContratos(req, res));
+  router.get("/contratos/:conjunto", (req, res) => controller.getContratos(req, res));
 
-  router.get("/rol-de-vacaciones", (req, res) =>
+  router.get("/rol-de-vacaciones/:conjunto", (req, res) =>
     controller.getRolDeVacaciones(req, res)
   );
 
-  router.get("/anualizado", (req, res) => controller.getAnualizado(req, res));
+  router.get("/anualizado/:conjunto", (req, res) => controller.getAnualizado(req, res));
 
-  router.get("/prestamo-cta-cte", (req, res) =>
+  router.get("/prestamo-cta-cte/:conjunto", (req, res) =>
     controller.getPrestamoCtaCte(req, res)
   );
 
