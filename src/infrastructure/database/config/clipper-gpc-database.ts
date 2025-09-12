226bd4abf21fd765ac69f1ce1cd0a2a5d8dc3e4c
GiancarloSanchez26 <sanchezgiancarlo82@gmail.com>
2025-09-12 09:29:28 -0700

    actualizando cambios de base de datos clipper contabilidad

--- src/infrastructure/database/config/clipper-gpc-database.ts
+++ src/infrastructure/database/config/clipper-gpc-database.ts
@@ -1,9 +1,9 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 dotenv.config();
 
 const clipperGPCSequelizeOptions = {
-  dialect: 'mssql' as const,
+  dialect: "mssql" as const,
   dialectOptions: {
     options: {
       encrypt: false,
@@ -20,7 +20,7 @@ const clipperGPCSequelizeOptions = {
 };
 
 const sequelizeClipperGPC = new Sequelize(
-  process.env["CLIPPER_GPC_DB_NAME"] || "CLIPPER_GPC_EMP009",
+  process.env["CLIPPER_GPC_DB_NAME"] || "CLIPPER_GPC_EMP001",
   process.env["CLIPPER_GPC_DB_USER"] || "",
   process.env["CLIPPER_GPC_DB_PASSWORD"] || "",
   {
@@ -29,8 +29,71 @@ const sequelizeClipperGPC = new Sequelize(
   }
 );
 
-const sequelizeClipperGPC1 = new Sequelize(
-  process.env["CLIPPER_GPC_DB_NAME"] || "IT2_TEST_1",
+const sequelizeClipperGPC2 = new Sequelize(
+  process.env["CLIPPER_GPC_DB_NAME"] || "CLIPPER_GPC_EMP002",
+  process.env["CLIPPER_GPC_DB_USER"] || "",
+  process.env["CLIPPER_GPC_DB_PASSWORD"] || "",
+  {
+    host: process.env["CLIPPER_GPC_DB_HOST"] || "localhost",
+    ...clipperGPCSequelizeOptions,
+  }
+);
+const sequelizeClipperGPC3 = new Sequelize(
+  process.env["CLIPPER_GPC_DB_NAME"] || "CLIPPER_GPC_EMP003",
+  process.env["CLIPPER_GPC_DB_USER"] || "",
+  process.env["CLIPPER_GPC_DB_PASSWORD"] || "",
+  {
+    host: process.env["CLIPPER_GPC_DB_HOST"] || "localhost",
+    ...clipperGPCSequelizeOptions,
+  }
+);
+const sequelizeClipperGPC4 = new Sequelize(
+  process.env["CLIPPER_GPC_DB_NAME"] || "CLIPPER_GPC_EMP004",
+  process.env["CLIPPER_GPC_DB_USER"] || "",
+  process.env["CLIPPER_GPC_DB_PASSWORD"] || "",
+  {
+    host: process.env["CLIPPER_GPC_DB_HOST"] || "localhost",
+    ...clipperGPCSequelizeOptions,
+  }
+);
+const sequelizeClipperGPC5 = new Sequelize(
+  process.env["CLIPPER_GPC_DB_NAME"] || "CLIPPER_GPC_EMP005",
+  process.env["CLIPPER_GPC_DB_USER"] || "",
+  process.env["CLIPPER_GPC_DB_PASSWORD"] || "",
+  {
+    host: process.env["CLIPPER_GPC_DB_HOST"] || "localhost",
+    ...clipperGPCSequelizeOptions,
+  }
+);
+const sequelizeClipperGPC6 = new Sequelize(
+  process.env["CLIPPER_GPC_DB_NAME"] || "CLIPPER_GPC_EMP006",
+  process.env["CLIPPER_GPC_DB_USER"] || "",
+  process.env["CLIPPER_GPC_DB_PASSWORD"] || "",
+  {
+    host: process.env["CLIPPER_GPC_DB_HOST"] || "localhost",
+    ...clipperGPCSequelizeOptions,
+  }
+);
+const sequelizeClipperGPC7 = new Sequelize(
+  process.env["CLIPPER_GPC_DB_NAME"] || "CLIPPER_GPC_EMP007",
+  process.env["CLIPPER_GPC_DB_USER"] || "",
+  process.env["CLIPPER_GPC_DB_PASSWORD"] || "",
+  {
+    host: process.env["CLIPPER_GPC_DB_HOST"] || "localhost",
+    ...clipperGPCSequelizeOptions,
+  }
+);
+const sequelizeClipperGPC8 = new Sequelize(
+  process.env["CLIPPER_GPC_DB_NAME"] || "CLIPPER_GPC_EMP008",
+  process.env["CLIPPER_GPC_DB_USER"] || "",
+  process.env["CLIPPER_GPC_DB_PASSWORD"] || "",
+  {
+    host: process.env["CLIPPER_GPC_DB_HOST"] || "localhost",
+    ...clipperGPCSequelizeOptions,
+  }
+);
+const sequelizeClipperGPC9 = new Sequelize(
+  process.env["CLIPPER_GPC_DB_NAME"] || "CLIPPER_GPC_EMP009",
   process.env["CLIPPER_GPC_DB_USER"] || "",
   process.env["CLIPPER_GPC_DB_PASSWORD"] || "",
   {
@@ -42,5 +105,12 @@ const sequelizeClipperGPC1 = new Sequelize(
 // Firma de índice para evitar error TS7053
 export const clipperGPCDatabases: { [key: string]: Sequelize } = {
   bdclipperGPC: sequelizeClipperGPC,
-  bdclipperGPC1: sequelizeClipperGPC1,
+  bdclipperGPC2: sequelizeClipperGPC2,
+  bdclipperGPC3: sequelizeClipperGPC3,
+  bdclipperGPC4: sequelizeClipperGPC4,
+  bdclipperGPC5: sequelizeClipperGPC5,
+  bdclipperGPC6: sequelizeClipperGPC6,
+  bdclipperGPC7: sequelizeClipperGPC7,
+  bdclipperGPC8: sequelizeClipperGPC8,
+  bdclipperGPC9: sequelizeClipperGPC9,
 };
--- src/infrastructure/database/config/hmis-database.ts
+++ src/infrastructure/database/config/hmis-database.ts
@@ -1,10 +1,10 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const hmisSequelizeOptions = {
-  dialect: 'mssql' as const,
+  dialect: "mssql" as const,
   dialectOptions: {
     options: {
       encrypt: false,
@@ -21,29 +21,60 @@ const hmisSequelizeOptions = {
 };
 
 // Conexión para la BD principal HMIS
-  const sequelizeHmis = new Sequelize(
-    process.env["HMIS_DB_NAME"] || "IT2_TEST",
-    process.env["HMIS_DB_USER"] || "",
-    process.env["HMIS_DB_PASSWORD"] || "",
-    {
-      host: process.env["HMIS_DB_HOST"] || "localhost",
-      ...hmisSequelizeOptions,
-    }
-  );
-  
-  const sequelizeHmis1 = new Sequelize(
-    process.env["HMIS1_DB_NAME"] || "IT2_TEST_1",
-    process.env["HMIS1_DB_USER"] || "",
-    process.env["HMIS1_DB_PASSWORD"] || "",
-    {
-      host: process.env["HMIS1_DB_HOST"] || "localhost",
-      ...hmisSequelizeOptions,
-    }
-  );
-  
+const sequelizeHmis = new Sequelize(
+  process.env["HMIS_DB_NAME"] || "IT2_TEST",
+  process.env["HMIS_DB_USER"] || "",
+  process.env["HMIS_DB_PASSWORD"] || "",
+  {
+    host: process.env["HMIS_DB_HOST"] || "localhost",
+    ...hmisSequelizeOptions,
+  }
+);
+
+const sequelizeHmisAQP = new Sequelize(
+  process.env["HMIS1_DB_NAME"] || "IT2_TEST_AQP",
+  process.env["HMIS1_DB_USER"] || "",
+  process.env["HMIS1_DB_PASSWORD"] || "",
+  {
+    host: process.env["HMIS1_DB_HOST"] || "localhost",
+    ...hmisSequelizeOptions,
+  }
+);
+
+const sequelizeHmisICA = new Sequelize(
+  process.env["HMIS1_DB_NAME"] || "IT2_TEST_ICA",
+  process.env["HMIS1_DB_USER"] || "",
+  process.env["HMIS1_DB_PASSWORD"] || "",
+  {
+    host: process.env["HMIS1_DB_HOST"] || "localhost",
+    ...hmisSequelizeOptions,
+  }
+);
+const sequelizeHmisPIURA = new Sequelize(
+  process.env["HMIS1_DB_NAME"] || "IT2_TEST_PIURA",
+  process.env["HMIS1_DB_USER"] || "",
+  process.env["HMIS1_DB_PASSWORD"] || "",
+  {
+    host: process.env["HMIS1_DB_HOST"] || "localhost",
+    ...hmisSequelizeOptions,
+  }
+);
+
+const sequelizeHmisTACNA = new Sequelize(
+  process.env["HMIS1_DB_NAME"] || "IT2_TEST_TACNA",
+  process.env["HMIS1_DB_USER"] || "",
+  process.env["HMIS1_DB_PASSWORD"] || "",
+  {
+    host: process.env["HMIS1_DB_HOST"] || "localhost",
+    ...hmisSequelizeOptions,
+  }
+);
 
 // Exportar las conexiones en un objeto para seleccionarlas por alias
 export const hmisDatabases = {
   bdhmis: sequelizeHmis,
-  bdhmis1: sequelizeHmis1,
+  bdhmisAQP: sequelizeHmisAQP,
+  bdhmisICA: sequelizeHmisICA,
+  bdhmisPIURA: sequelizeHmisPIURA,
+  bdhmisTACNA: sequelizeHmisTACNA,
 };
